4f6203a6d228dd0b72706e9939e23f0ccf5c7e41
Felippex <55957871+Felippex@users.noreply.github.com>
2020-09-09 19:53:13 +0200

    implement bigger/smaller restriction

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 const rings = document.querySelectorAll('.circle')
 const emptyPlaces = document.querySelectorAll('.stick')
 
+const firstRing = document.querySelector('.circle1');
 
 rings.forEach(ring => {
   ring.addEventListener('dragstart', () => {
-    ring.classList.add('dragging')
+    ring.classList.add('dragging');
   })
-})
-rings.forEach(ring => {
   ring.addEventListener('dragend', () => {
     ring.classList.remove('dragging')
   })
+
 })
 
-
 // drag functions
-
 emptyPlaces.forEach(emptyPlace => {
   emptyPlace.addEventListener('dragover',
     e => {
@@ -23,44 +21,22 @@ emptyPlaces.forEach(emptyPlace => {
     })
   emptyPlace.addEventListener('drop', () => {
     const ring = document.querySelector('.dragging')
+    if ((emptyPlace.childElementCount > 0) && (ring.id > emptyPlace.children[0].id)) {
+      return alert('niedozwolony ruch')
+    }
     emptyPlace.prepend(ring)
-    const newRing = document.querySelector('.circle')
-    newRing.draggable = 'true'
+
+    rings.forEach(ring => {
+      if (ring.previousElementSibling) {
+        ring.draggable = false
+      } else {
+        ring.draggable = true
+      }
+    })
+    if (emptyPlace.children.length > 1) {
+
+    }
   })
 
+
 })
-
-
-// function dragStart() {
-//   ring.classList.add('dragging')
-//   setTimeout(() => (this.classList.add('invisible')), 0);
-// }
-
-// function dragEnd() {
-//   ring.classList.remove('dragging')
-//   this.classlist.remove('invisible');
-// }
-
-// function dragOver(e) {
-//   e.preventDefault()
-//   const ring = document.querySelector('dragging')
-//   emptyPlaces.prepend(ring)
-// }
-
-// function dragEnter() {}
-
-// function dragLeave() {}
-
-// function dragDrop() {
-//   rings.forEach(ring => console.log(ring))
-//   console.log(this, rings);
-// }
-
-
-
-// emptyPlaces.forEach((emptyPlace) => {
-//   emptyPlace.addEventListener('dragover', dragOver)
-//   emptyPlace.addEventListener('dragenter', dragEnter)
-//   emptyPlace.addEventListener('dragleave', dragLeave)
-//   emptyPlace.addEventListener('drop', dragDrop)
-// })
